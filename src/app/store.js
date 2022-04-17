7a03f5b0d23d7f5f161aecf850697b33dd7d9261
Dev bro <tomakeitworth@gmail.com>
2022-04-17 05:24:26 +0500

    connect CartReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import CartReducer from '../features/Cart';
 
 export const store = configureStore({
   reducer: {
+    cart : CartReducer
   },
 });
