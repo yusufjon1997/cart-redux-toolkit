b4e1cc3f8c57a69a0536c2339c1a50c81783141c
Dev bro <tomakeitworth@gmail.com>
2022-04-17 05:52:17 +0500

    use react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import './App.css';
+import Cart from './components/Cart';
+import Products from './components/Products';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Products/>} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
